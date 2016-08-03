74d0c95411fe3023bf5a8dbd45ae9167ab2eca66
vmehera123 <v.mehera123@gmail.com>
2016-08-03 16:45:05 +0300

    03.08.2016

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require("express"),
     app = express(),
     path = require("path");
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname + "/static")));
 app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(8888, function() {
+app.listen("127.0.0.1:8000", function() {
   console.log("SERVER IS RUNNING");
 });
