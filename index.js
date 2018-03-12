711a5e66b1057629e5b0204009d050ffbec231a7
vmehera123 <v.mehera123@gmail.com>
2018-03-12 10:12:55 +0200

    12.03.2018 add robots.txt and sitemap handlers

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get("/about", function (req, res) {
 	res.sendFile(path.resolve(__dirname, "./views/about.html"));
 });
 
+app.get("/robots.txt", function (req, res) {
+	res.sendFile(path.resolve(__dirname, "./robots.txt"));
+});
+
+app.get("/sitemap.xml", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./sitemap.xml"));
+});
+
 app.listen(8000, function() {
   	console.log("SERVER IS RUNNING");
 });
